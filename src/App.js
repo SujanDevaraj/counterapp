5659712bdccd29aaeffcab6985a7f8909dd9aa85
SujanDevaraj <sujand072@gmail.com>
2023-12-19 14:15:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Counter from "./Components/Counter.js";
+import "./style.css";
+const App=()=>{
+
+
+    return (
+        <Counter/>
+    )
+}
+export default App;
